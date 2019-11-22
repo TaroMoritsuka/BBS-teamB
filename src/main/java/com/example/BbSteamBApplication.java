69d1b86d0ab00d833a5528f02a1ed325b787ffda
TaroMoritsuka <pepperoni.4.eg@gmail.com>
2019-11-22 13:40:11 +0900

    first commit

---
+++ src/main/java/com/example/BbSteamBApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbSteamBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbSteamBApplication.class, args);
+	}
+
+}
