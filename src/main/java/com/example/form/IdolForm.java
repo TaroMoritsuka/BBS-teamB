0f460d818f4fa83f7b1287b5807b43c4cce11cf6
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-22 14:03:52 +0900

    domain

---
+++ src/main/java/com/example/form/IdolForm.java
@@ -0,0 +1,71 @@
+package com.example.form;
+
+public class IdolForm {
+
+	private String id;
+	private String name;
+	private String age;
+	private String birthPlace;
+
+	@Override
+	public String toString() {
+		return "Form [id=" + id + ", name=" + name + ", age=" + age + ", birthPlace=" + birthPlace + "]";
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public String getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the birthPlace
+	 */
+	public String getBirthPlace() {
+		return birthPlace;
+	}
+
+	/**
+	 * @param birthPlace the birthPlace to set
+	 */
+	public void setBirthPlace(String birthPlace) {
+		this.birthPlace = birthPlace;
+	}
+
+}
